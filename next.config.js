a78486fea0c0e7e008e43ebc0c4ddb670cee972a
davexxx <davexxx@163.com>
2023-09-12 00:31:32 +0800

    fix build issue

--- next.config.js
+++ next.config.js
@@ -2,8 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  webpack(config) {
+  webpack: (config) => {
     config.experiments = { ...config.experiments, topLevelAwait: true };
+    config.resolve = {
+      ...config.resolve,
+      fallback: {
+        "fs": false,
+        "path": false,
+        "os": false,
+      }
+    }
     return config;
   },
 };

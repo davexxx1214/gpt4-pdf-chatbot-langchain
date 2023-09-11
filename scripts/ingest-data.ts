0a22d1390213b34b0f8754a5289d3dcbc3c915c0
davexxx <davexxx@163.com>
2023-09-11 22:31:21 +0800

    enable double enter. add summary for ingest. add upload file dialog for multiple files.

--- scripts/ingest-data.ts
+++ scripts/ingest-data.ts
@@ -6,6 +6,11 @@ import { PDFLoader } from 'langchain/document_loaders/fs/pdf';
 import { PINECONE_INDEX_NAME, PINECONE_NAME_SPACE } from '@/config/pinecone';
 import { DirectoryLoader } from 'langchain/document_loaders/fs/directory';
 
+import { OpenAI } from "langchain/llms/openai";
+import { loadSummarizationChain } from "langchain/chains";
+import { Document } from "langchain/document";
+
+
 /* Name of directory to retrieve your files from 
    Make sure to add your PDF files inside the 'docs' folder
 */
@@ -23,17 +28,40 @@ export const run = async () => {
 
     /* Split text into chunks */
     const textSplitter = new RecursiveCharacterTextSplitter({
-      chunkSize: 1000,
-      chunkOverlap: 200,
+      chunkSize: 8000,
+      chunkOverlap: 100,
     });
 
     const docs = await textSplitter.splitDocuments(rawDocs);
-    console.log('split docs', docs);
+    // console.log('split docs', docs);
 
     console.log('creating vector store...');
     /*create and store the embeddings in the vectorStore*/
-    const embeddings = new OpenAIEmbeddings();
+    const embeddings = new OpenAIEmbeddings({maxConcurrency: 10});
     const index = pinecone.Index(PINECONE_INDEX_NAME); //change to your own index name
+
+
+    const model = new OpenAI({ modelName: 'gpt-3.5-turbo-16k', temperature: 0 });
+
+    // This convenience function creates a document chain prompted to summarize a set of documents.
+    const chain = loadSummarizationChain(model, {
+      type: "map_reduce",
+      returnIntermediateSteps: true,
+    });
+    const res = await chain.call({
+      input_documents: docs,
+    });
+
+    const summary_content = "The summary of this file is: " + res.text as string;
+    console.log(summary_content);
+    const summary = new Document({ pageContent : summary_content});
+    docs.push(summary);
+
+    // Delete all vectors in namespace
+    await index.delete1({
+      deleteAll: true,
+      namespace: PINECONE_NAME_SPACE
+    });
 
     //embed the PDF documents
     await PineconeStore.fromDocuments(docs, embeddings, {

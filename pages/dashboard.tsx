1e02c14e7bdd9d0aa41c878720ee559770b9de8f
davexxx <davexxx@163.com>
2023-09-15 16:53:59 +0800

    update dashboard.

---
+++ pages/dashboard.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Layout from '@/components/layout';
+
+const Dashboard = () => {
+  return (
+    <>
+      <Layout>
+        <div className="center">
+          <iframe src="http://4.194.18.191:3000/d-solo/ed80b278-8649-45cb-827b-81a6863ea1a1/abi-model-v2?orgId=1&from=1694740538679&to=1694762138679&panelId=1" width="600" height="500" ></iframe>
+        </div>
+      </Layout>
+    </>
+
+  );
+};
+export default Dashboard;

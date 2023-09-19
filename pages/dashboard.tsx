739fc866ae207e36c70ab59f2878082a9c9d7df6
davexxx <davexxx@163.com>
2023-09-19 18:00:09 +0800

    update dashboard

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -6,7 +6,7 @@ const Dashboard = () => {
     <>
       <Layout>
         <div className="center">
-          <iframe src="http://4.194.18.191:3000/d-solo/ed80b278-8649-45cb-827b-81a6863ea1a1/abi-model-v2?orgId=1&from=1694740538679&to=1694762138679&panelId=1" width="600" height="500" ></iframe>
+          <iframe src="http://20.189.79.55:3000/d-solo/cd352c5e-6801-481c-82e0-b5f5275b4205/ss-absolute-return-model?orgId=1&from=1695094889520&to=1695116489520&theme=light&panelId=1"  width="100%" height="100%"></iframe>
         </div>
       </Layout>
     </>

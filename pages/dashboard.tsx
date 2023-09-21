60f2fe27972a3158ac71e00a84d4e77992396623
davexxx <davexxx@163.com>
2023-09-21 10:22:27 +0800

    update dashboard.

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -6,7 +6,7 @@ const Dashboard = () => {
     <>
       <Layout>
         <div className="center">
-          <iframe src="http://20.189.79.55:3000/d/f417d4a7-4d6f-4aaa-afcd-56afc0e4ae99/ss-overall-models?orgId=1&from=1695180484703&to=1695202084703"  width="100%" height="800px"></iframe>
+          <iframe src="http://20.24.158.53:3000/d/fViuhCiIk/modelsdashboard?orgId=1&refresh=5s&from=1695241247699&to=1695262847700"  width="100%" height="800px"></iframe>
         </div>
       </Layout>
     </>

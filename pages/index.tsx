032bfe901b8a55346c20d2422c01d63a26521570
davexxx <davexxx@163.com>
2023-09-22 00:22:54 +0800

    Revert "update background"
    This reverts commit d8ec7ee1f3723f488db41d3cad3066c5790cafaa.

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,17 +7,16 @@ export default function Home() {
     <>
       <Layout>
         <img
-          src="/backgroud.png" alt="AILogo"
+          src="/backgroud1.png" alt="AILogo"
         />
-        <div className="absolute w-full h-200 top-0 bottom-0 left-0 right-0 flex items-center justify-end gap-4">
-          <div className="flex flex-col items-center">
+        <div className="absolute w-full h-full top-0 bottom-0 left-10 right-0 flex items-center justify-left">
+          <div className="flex flex-col items-left">
             <span className="text-white text-3xl break-words">Leverage our management experience</span>
             <span className="text-white text-3xl break-words">Enhance your investing experience</span>
           </div>
         </div>
-        <div className="absolute w-full h-300 top-21 bottom-0 left-0 right-0 flex items-center justify-center gap-10">
+        <div className="absolute w-full h-full top-40 bottom-0 left-10 right-0 flex items-center justify-left">
           <Link className="text-xl px-4 py-2 rounded text-blue-600 bg-white hover:bg-gray-600" href="/chat">Talk with AI</Link>
-          <Link className="text-xl px-4 py-2 rounded text-blue-600 bg-white hover:bg-gray-600" href="/chat">Manage Your Data</Link>
         </div>
       </Layout>
     </>

d8ec7ee1f3723f488db41d3cad3066c5790cafaa
davexxx <davexxx@163.com>
2023-09-22 00:01:10 +0800

    update background

--- pages/azure.tsx
+++ pages/azure.tsx
@@ -1,11 +1,21 @@
 import { useState } from 'react';
 import Layout from '@/components/layout';
+import MultipleFileUploadForm from "@/components/ui//MultipleFileUploadForm";
 import { toast, ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 
 
 export default function Azure() {
     const [uploading, setUploading] = useState<boolean>(false);
+    const [cleanDB, setCleanDB] = useState<boolean>(false);
+    globalThis._cleanDB = cleanDB;
+
+    const handleChange = (e: { target: { checked: any; }; }) => {
+        const { checked } = e.target;
+        setCleanDB(checked);
+        globalThis._cleanDB = checked;
+        console.log("handleChange = " + globalThis._cleanDB);
+    }
 
     const onProcess = async () => {
         toast('Sync started.', { hideProgressBar: true, autoClose: 2000, type: 'success', position: 'top-center' });
@@ -48,13 +58,30 @@ export default function Azure() {
         <>
             <Layout>
                 <img src="/azure.png" alt="Azure" />
+                <div className="absolute w-full h-500 top-40 bottom-0 left-10 right-0 flex items-center justify-left">
 
-                <div className="absolute w-full h-full top-40 bottom-0 left-10 right-0 flex items-center justify-left">
+                <div className="mx-auto flex flex-col gap-4">
+                    <main className="py-10">
+                        <div className="w-full max-w-3xl px-3 mx-auto">
+                            <div className="space-y-10">
+                                <div>
+                                    <h2 className="mb-3 text-xl font-bold text-gray-900">
+                                        File Upload
+                                    </h2>
+                                    <MultipleFileUploadForm />
+                                </div>
+                            </div>
+                        </div>
+                    </main>
+                    
+                </div>
+                </div>
+
+                <div className="absolute w-full h-200 top-21 bottom-0 left-10 right-0 flex items-center justify-center">
                     <div className="mx-auto flex flex-col gap-4">
                         <main className="py-10">
                             <div className="w-full max-w-3xl px-3 mx-auto">
                                 <div className="space-y-10">
-                                    <div>
                                         <button
                                             disabled={uploading}
                                             onClick={onProcess}
@@ -63,7 +90,6 @@ export default function Azure() {
                                             Sync from Azure storage container
                                         </button>
 
-                                    </div>
                                 </div>
                             </div>
                         </main>
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,16 +7,17 @@ export default function Home() {
     <>
       <Layout>
         <img
-          src="/backgroud1.png" alt="AILogo"
+          src="/backgroud.png" alt="AILogo"
         />
-        <div className="absolute w-full h-full top-0 bottom-0 left-10 right-0 flex items-center justify-left">
-          <div className="flex flex-col items-left">
+        <div className="absolute w-full h-200 top-0 bottom-0 left-0 right-0 flex items-center justify-end gap-4">
+          <div className="flex flex-col items-center">
             <span className="text-white text-3xl break-words">Leverage our management experience</span>
             <span className="text-white text-3xl break-words">Enhance your investing experience</span>
           </div>
         </div>
-        <div className="absolute w-full h-full top-40 bottom-0 left-10 right-0 flex items-center justify-left">
+        <div className="absolute w-full h-300 top-21 bottom-0 left-0 right-0 flex items-center justify-center gap-10">
           <Link className="text-xl px-4 py-2 rounded text-blue-600 bg-white hover:bg-gray-600" href="/chat">Talk with AI</Link>
+          <Link className="text-xl px-4 py-2 rounded text-blue-600 bg-white hover:bg-gray-600" href="/chat">Manage Your Data</Link>
         </div>
       </Layout>
     </>

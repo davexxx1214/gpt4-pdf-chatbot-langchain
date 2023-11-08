cffcbe1aed18cc97eb68222b45b2bc4625d417e8
davexxx <davexxx@163.com>
2023-11-08 23:48:29 +0800

    update prompt

--- pages/api/chat.ts
+++ pages/api/chat.ts
@@ -11,7 +11,7 @@ export default async function handler(
   res: NextApiResponse,
 ) {
   const {question, history } = req.body;
-  const defaultPrompt = "你是一名文化旅游店铺导购方面的专家，擅长用生动简洁的语言给人们推荐美食，旅游景点。推荐的时候尽量给出具体的店铺名称及相应评价。如果上下文中包含JSON格式，那么里面的条目代表汇智国际商业中心店的店铺，条目之间相互独立。其中name的值代表店铺名称，avgPrice的值对象中，人均后面的数字代表人均消费，recommend的值代表该店铺的推荐菜的列表，address的值代表店铺地址，tel的值代表店铺电话，comment的值代表用户评价。请优先从以下提示中寻找到答案";
+  const defaultPrompt = "你是一名文化旅游店铺导购方面的专家，擅长用生动简洁的语言给人们推荐美食，旅游景点。推荐的时候尽量给出具体的店铺名称及相应评价。店铺信息里将包含JSON格式，里面的条目代表汇智国际商业中心店的店铺，条目之间相互独立。其中name的值代表店铺名称，avgPrice的值代表人均消费，recommend的值代表该店铺的推荐菜列表，address的值代表店铺的详细地址，tel的值代表店铺电话，comment的值代表用户评价,stars的值代表用户评分。请优先推荐用户评分最高的店铺名,并列举出用户评分和详细地址及推荐理由。店铺信息如下：";
   let {prompt} = req.body;
   console.log('prompt', prompt);
   console.log('question', question);
--- utils/makechain.ts
+++ utils/makechain.ts
@@ -2,12 +2,7 @@ import { OpenAI } from 'langchain/llms/openai';
 import { PineconeStore } from 'langchain/vectorstores/pinecone';
 import { ConversationalRetrievalQAChain } from 'langchain/chains';
 
-const CONDENSE_PROMPT = `Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.
-
-Chat History:
-{chat_history}
-Follow Up Input: {question}
-Standalone question:`;
+const CONDENSE_PROMPT = `{question}`;
 
 const QA_PROMPT = `
 {context}
